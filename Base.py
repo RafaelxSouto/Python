52f62c8419a4b26a8e0c69531d239c067833579b
Rafael Souto <143662204+RafaelxSouto@users.noreply.github.com>
2023-10-25 19:07:05 -0300

    Create Base.py

---
+++ Base.py
@@ -0,0 +1,66 @@
+# -> Comentário
+
+'''DocString'''
+
+# \r\n -> CRLF -> Quebra de linha Windows
+# \n -> LF ->Quebra de linha Mac/Linux
+
+# sep='*' -> Separador
+# end=' ' -> Quebra de linha (remove)
+
+# type() -> mostra o tipo
+
+# Prioridade Aritmetica
+# 1° (n + n)
+# 2° **
+# 3° * / // %
+# 4° + -
+
+# FomartString
+# print(f'hi, {name}')
+# print('hi, {}'.format(name))
+
+# Condicionais
+# if / elif      / else
+# se / se não se / se não
+
+# Operadores de Caomparação
+# >  -> maior que (2 > 1) 
+# >= -> maior ou igual (2 >= 2)
+# <  -> menor que (2 < 3)
+# <= -> menor ou igual (3 <= 3)
+# == -> igual a (2 == 2)
+# != -> diferente (2 != 3)
+
+# Operadores Lógicos
+# and ->
+# or  ->
+# not ->
+# not True -> False
+# not False -> True
+# in  -> encontrar algo no local
+# not in -> não encontrei
+
+# Fatiamento de String
+# print(nome[i:f:p]) 
+# print(nome[1:10:2]) 
+
+# len() => Retorna a quantidade de string no local
+
+# while -> Enquanto for True continuar rodando
+
+# Operadores de Atribuição
+# = += -= *= /= //= **= %=
+# contador = 0
+# contador += 1
+
+# try
+# except
+
+# for x in var:
+#   ...
+
+# List in Python
+
+# def -> Definir uma função
+# global puxa externo para interno
